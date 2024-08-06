2c361e7d8507b840685b936499cce9e42931b139
Florian Keppeler <floke.eklof@googlemail.com>
2024-08-06 13:43:27 +0200

    Powerpoint für Abstimmung erstellt

---
+++ Struktur.R
@@ -0,0 +1,83 @@
+#--------------------------------------------------------------
+# R Kurs: Tag 1
+#-------------------------------------------------------------
+
+# Vorstellung meiner Person: Bachelor und Master in Umweltwissenschaften Freiburg
+# seit 2018 an der Forstlichen Versuchs- und Forschungsanstalt BW in der Abteilung
+# Biometrie und Informatik beschäftigt, erst als Hiwi und später als Wissenschaftl.
+# Mitarbeiter mit dem Schwerpunkt auf Datenanalyse und Modellierung, alles in R.
+# Viel mit räumlichen Daten gearbeitet und in der internen Statistikberatung der 
+# FVA mitgewirkt. Seit Anfang diesen Jahres arbeite ich an der Professur für 
+# Waldwachstum und Dendroökologie als Software- entwickler für das Modell WEHAM, 
+# allerdings in Teilzeit da ich seit Oktober letzten Jahres ein Teilzeitstudium 
+# in Informatik absolviere. Fast 10 Jahre Erfahrung mit R durch das Informatikstudium
+# ist es mir noch sehr präsent wie frustrierend der Erstkontakt mit einer neuen
+# Programmiersprache sein kann.
+
+# [Optional]: Frage nach den individuellen Erfahrungen der Teilnehmenden mit 
+# Programmierung oder Datenauswertung
+# Warum? -> Für mich damit ich ein Gespür bekomme auf welchem Niveau alle sind
+# vielleicht auch um abschätzen zu können welche Fragestellungen interessant sind
+
+# Was passiert im Kurs?
+
+# 3 Tage Zeit um uns so weit mit der Programmiersprache R und der Entwicklungs-
+# umgebung RStudio vetraut zu machen, dass danach eigene Projekte umgesetzt
+# werden können
+# Warum? ->
+
+# Mischung aus PowerPoint Folien, Live Coding und vor allem Übungen
+# Warum? -> Folien sind gut im schnell viel Inhalte zu vermitteln, das bedeutet
+# aber nicht, dass die Inhalte dadurch besser aufgenommen werden können. Daher 
+# Live Coding: natürliche Limitation wie viel Inhalt ich präsentieren kann und 
+# im besten Fall bleibt alles nachvollziehbar. Das wichtigste ist aber, eigene 
+# Erfahrungen zu machen und im besten Fall alle Fehler die man machen kann einmal
+# selbst machen und Strategien entwickeln mit diesen Situationen umzugehen. Das
+# kann ganz praktisch sein, über das lernen richtig zu googeln, oder eher emotional
+# indem man sich eine gewisse Frustrationstoleranz aneignet. Gerade die Frustrations-
+# toleranz ist absolut nicht zu unterschätzen
+
+# Ablauf: 
+#  9:45 - 13:00 Neue Inhalte mit kleineren Übungen um die 10 Minuten
+# 14:00 - 15:30 betreute Übung zu den vorgestellten Inhalten + anschließende Besprechung
+
+
+# Inhalte
+
+# Datentypen in R
+# -> Variablen, Vektoren, Matrizen, Data.frames und andere Objekte
+
+# Daten aufbereiten
+# -> Filtern, Auswählen und editieren
+
+# Funktionen
+# -> Anwendung und eigene Funktionen schreiben; Hilfe zu Funktionen
+
+# R Packages
+# -> Wie installieren und nutzen
+
+# beschreibende Datenanalyse
+# -> Mittelwerte, Häufigkeiten, Minimum, Maximum, Summen etc. und deren Darstellung
+# als Grafiken
+# -> Einführung zu R Plots
+
+# Interaktion mit dem Dateiensystem des Betriebssystems
+# -> Daten einlesen und schreiben, Ordner und Dateien erzeugen, kopieren, löschen
+# Inhalte von Verzeichnissen auslesen
+
+# Automatisierung
+# -> Schleifen (While und For); Ganze Skripte automatisieren mit Schleifen und
+# Funktionen -> wie gehe ich das an?; Paste Befehl!
+
+# [Optional]: Gedodaten in R -> Wie viel Wissen ist hier vorraussetzbar?
+# -> Packages, einladen von Geodaten, Prozessierung von Vektor und Rasterdaten
+# eventuell Verschneidung von Raster und Vektor zur Erstellung eines statistischen
+# Modells -> Vorhersage als Karte
+
+# [Optional]: statistische Modelle in R -> Wie viel Wissen ist hier vorraussetzbar?
+# -> lineare Modelle, generalisierte lineare Modell, Additive Modelle, 
+# Mashine Learning fitten, analysieren und Vorhersagen erzeugen
+
+# [Optional]: R Markdown -> R und Latex
+# von Interesse? Spielt Latex überhaupt eine Rolle? 
+
